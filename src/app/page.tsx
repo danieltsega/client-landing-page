ccdfec41d541fd412915ff50713eb03f409e58f9
danieltsega <danieltsega6658@gmail.com>
2025-11-11 05:52:50 -0500

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/sections/Hero";
 import Navbar from "@/components/ui/Navbar";
 
 export default function Home() {
   return (
     <main className="relative">
       <Navbar />
-
+      <Hero />
       {/* Keep your 5 test sections */}
       {[...Array(5)].map((_, i) => (
         <section
